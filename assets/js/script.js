6ca1c0ad2943153fb7cea0ad83a73cf7e8c894fc
Hamzah <hamzahullah@yahoo.com>
2021-05-21 15:51:19 -0400

    Create array object of question answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log("test")
+function questions(){
+    var questions = [
+        {q:"How do you iterate through an array?", a:"for loops"},
+        {q:"Conditional Statement considering 'either or", a:"if/else"},
+        {q:"Continues running till all parameters are met, may complete an action before running", a:"do..while" },
+        {q:"Ends looping, regardless of conditions met", a:"break" },
+        {q:"Keeps moving through iterations, ignoring code below it", a:"continue"},
+        {q:"Emmet code to quickly enter initial index code", a:"html:5"},
+        {q:"Append array in javascript", a:"push"},
+        {q:"Removes last element of an array", a:"pop"},
+        {q:"Position of element in an array", a:"index"},
+        {q:"Terminology used to change datatypes", a:"parse"}
+    ]
+    return questions
+}
+console.log(questions())
