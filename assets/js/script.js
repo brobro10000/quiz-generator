faf19a4aec742aa8946b2de7836a5ecfdc47f051
Hamzah <hamzahullah@yahoo.com>
2021-05-21 17:27:02 -0400

    Created Random unique order array to generate unique questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 function questions(){
-    var questions = [
+    var questionArr = [
         {q:"How do you iterate through an array?", a:"for loops"},
         {q:"Conditional Statement considering 'either or", a:"if/else"},
         {q:"Continues running till all parameters are met, may complete an action before running", a:"do..while" },
@@ -11,6 +11,46 @@ function questions(){
         {q:"Position of element in an array", a:"index"},
         {q:"Terminology used to change datatypes", a:"parse"}
     ]
-    return questions
+    // if(attr == 'q')
+    // return questionArr[i].q
+    // else if(attr =='a')
+    // return questionArr[i].a
+
+    return questionArr
 }
+function questionOrder() {
+    var questionArr = questions()
+    var qOrder = []
+    var min = 0
+    var max = 9
+    var number 
+    while(qOrder.length < questionArr.length)
+    {
+        number = getRandom(min,max)
+        if(qOrder.indexOf(number) == -1)
+        qOrder.push(number)
+    }
+    console.log(qOrder)
+    return qOrder
+}
+function generateQuestion(i)
+{
+    var questionArr = questions()
+    var randomOrder = questionOrder()
+    var body = document.body
+    var questionEl = document.createElement("h2")
+    
+    questionEl.textContent = questionArr[randomOrder[i]].q
+
+    body.appendChild(questionEl)
+}
+//Random number generator inclusive
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
 console.log(questions())
+questionOrder()
+questionArr = questions()
+for(var i = 0; i< questionArr.length; i++)
+generateQuestion(i)
